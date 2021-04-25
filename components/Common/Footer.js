c5e73835d2804a4eec84c4f0091aa11b256e3f08
Chris Held <cheld100@gmail.com>
2021-04-25 17:57:12 -0500

    update footer link

--- components/Common/Footer.js
+++ components/Common/Footer.js
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
     <footer>
-      <a href="#">
+      <a
+        href="https://github.com/chris-held/elixirforajsdev/issues"
+        target="_blank"
+      >
         <p className="text-center underline">
           Something you'd like to see? File an issue on github!
         </p>
