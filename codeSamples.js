f2d75e7c7fab0abcfb14d46381bf2ca370ce1054
Chris Held <cheld100@gmail.com>
2021-05-10 18:08:35 -0500

    update examples

--- codeSamples.js
+++ codeSamples.js
@@ -1,6 +1,28 @@
 // code samples that aren't one liners can go here to
 // keep other parts of the app clean
 
+export const jsVars = `
+ const foo = 1;
+ let bar = 2;
+ var baz = 3;
+
+`;
+
+export const elixirVars = `
+ foo = 1
+ # atoms are elixir variables that are used to
+ # represent a constant whose value is its own name
+ # they are often used to express status
+ :ok
+ :error
+
+`;
+
+export const jsImports = `
+ import React, { useState } from 'react';
+
+`;
+
 export const elixirImports = `
  # in elixir, you can use a fully qualified name without importing,
  # or you can import like this:
@@ -22,11 +44,16 @@ export const jsFunction = `
 
 export const elixirFunction = `
  def add(n1, n2) do
-     n1 + n2
+  n1 + n2
  end
 
  add = fn(a, b) -> a + b end
  add = &(&1 + &2)
+
+ # functions in elixir can be private
+ defp private_add(n1, n2) do
+  n1 + n2
+ end
  
 `;
 
@@ -52,7 +79,7 @@ export const elixirFilter = `
 
 export const jsFilter = `
  const people = [{name: 'Bob', age: 30}, {nane: 'Bill', age: 18}];
- const oldEnough = a.filter({age} => age > 21);
+ const oldEnough = people.filter({age} => age > 21);
 
 `;
 
@@ -65,5 +92,136 @@ export const elixirReduce = `
 export const jsReduce = `
  const sum = [1, 2, 3].reduce((acc, n) => n + acc, 0)
  // sum is 6
+
+`;
+
+export const jsChaining = `
+ const people = [{name: 'Bob', age: 30}, {nane: 'Bill', age: 18}];
+ const filteredAndMapped = people.filter({age} => age > 21).amp({name} => name);
+
+ // ['Bob']
+
+`;
+
+export const elixirChaining = `
+ people = [%{name: "Bob", age: 30}, %{name: "Bill", age: 18}]
+ old_enough_names = Enum.filter(people, fn %{age: age} -> age > 21 end)
+ |> Enum.map(n, fn n -> n.name end)
+
+ # ["Bob"]
+
+ # The pipe operator (|>) can be applied to more than just arrays:
+ score = 45
+ score
+ |> Kernel./(2)
+ |> :math.pow(-1)
+ |> Kernel.*(100)
+
+ # equivalent to (((45 / 2) ^ -1) * 100)
+
+`;
+
+export const jsDestructuring = `
+ const o = { nested: { prop: 'Hi!' } };
+
+ const { nested: { prop } = {} } = o;
+
+ console.log(prop);
+ // Hi!
+
+`;
+
+export const jsDestructuring2 = `
+ const list = ( user ) => {
+   if (user.isAdmin) {
+     return store.listAll();
+   }
+   return store.listForUser(user);
+ }
+
+`;
+
+export const elixirDestructuring = `
+ o = %{ nested: %{ prop: "Hi!" }}
+
+ %{ nested: %{ prop: prop }} = o
+
+ IO.inspect(prop)
+
+ # Hi!
+
+`;
+
+export const elixirDestructuring2 = `
+ # if the user passed in is an admin, this
+ # function will be called
+ defp list( %{ is_admin = true } ) do
+  store.list_all()
+ end
+
+ # regular users have this function called
+ defp list( user ) do
+  store.list_for_user(user)
+ end
+
+`;
+
+export const jsCase = `
+ const response = getAResponse();
+ switch (response.status) {
+   case 200:
+     return "Success";
+   case 401:
+     return "Not Allowed";
+   default:
+     return "There was an error";
+ }
+
+`;
+
+export const elixirCase = `
+ # very common in elixir to use case statements
+ # to match on the results of a function. In this
+ # example get_a_response returns a tuple where the
+ # first element is the status
+ case get_a_repoonse do
+  { :ok, _ } ->
+    "Success"
+  { :error, %{ reason = "Unauthorized" } } ->
+    "Not Allowed"
+  { :error, _ } ->
+    "There was an error"
+ end
+
+`;
+
+export const jsIf = `
+ if (n > 100) {
+   return 100;
+ } else {
+   return n;
+ }
+
+ n > 100 ? 100 : n;
+
+`;
+
+export const elixirIf = `
+ if n > 100 do
+  100
+ else
+  n
+ end
+
+ # unless keyword is a sort of reverse if
+ unless n > 100 do
+  n
+ else
+  100
+ end 
+ 
+ # ternary isn't really directly supported, but this version of
+ # an if statement is one line and very easy to read
+ if n > 100, do: 100, else: n
 
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from "next/head";
 import {
+  elixirCase,
+  elixirChaining,
+  elixirDestructuring,
+  elixirDestructuring2,
   elixirFilter,
   elixirFunction,
+  elixirIf,
   elixirImports,
   elixirMap,
   elixirReduce,
+  elixirVars,
+  jsCase,
+  jsChaining,
+  jsDestructuring,
+  jsDestructuring2,
   jsFilter,
   jsFunction,
+  jsImports,
+  jsIf,
   jsMap,
   jsReduce,
+  jsVars,
 } from "../codeSamples";
 import CodeComparison from "../components/CodeComparison/CodeComparison";
 import CodeComparisonDescription from "../components/CodeComparison/CodeComparisonDescription";
@@ -41,16 +54,13 @@ export default function Home() {
           <Collabsible title="Basics" startsOpen>
             <>
               <CodeComparisonHeader>Variable Declaration</CodeComparisonHeader>
-              <CodeComparison js="const foo = 1;" elixir="foo = 1" />
+              <CodeComparison js={jsVars} elixir={elixirVars} />
               <CodeComparisonDescription>
                 Unlike JS, all elixir data is immutable and must be assigned a
                 value at the time the variable is declared.
               </CodeComparisonDescription>
               <CodeComparisonHeader>Imports</CodeComparisonHeader>
-              <CodeComparison
-                js="import React, { useState } from 'react';"
-                elixir={elixirImports}
-              />
+              <CodeComparison js={jsImports} elixir={elixirImports} />
               <CodeComparisonHeader>Functions</CodeComparisonHeader>
               <CodeComparison js={jsFunction} elixir={elixirFunction} />
               <CodeComparisonDescription>
@@ -63,7 +73,64 @@ export default function Home() {
                   here.
                 </a>
               </CodeComparisonDescription>
+              <CodeComparisonHeader>Method Chaining</CodeComparisonHeader>
+              <CodeComparison js={jsChaining} elixir={elixirChaining} />
+              <CodeComparisonDescription>
+                The pipe operator is a very powerful tool in Elixir - read more
+                about it{" "}
+                <a
+                  target="_blank"
+                  className="underline"
+                  href="https://elixirschool.com/en/lessons/basics/pipe-operator/"
+                >
+                  here.
+                </a>
+              </CodeComparisonDescription>
+              <CodeComparisonHeader>Destructuring</CodeComparisonHeader>
+              <CodeComparison
+                js={jsDestructuring}
+                elixir={elixirDestructuring}
+              />
+              <CodeComparisonDescription>
+                <a
+                  target="_blank"
+                  className="underline"
+                  href="https://elixir-lang.org/getting-started/pattern-matching.html"
+                >
+                  Pattern Matching
+                </a>{" "}
+                is the Elixir equivalent of destructuring. In Elixir it is
+                common to have multiple functions defined with the same name
+                that match on different properties:
+              </CodeComparisonDescription>
+              <CodeComparison
+                js={jsDestructuring2}
+                elixir={elixirDestructuring2}
+              />
+              <CodeComparisonDescription>
+                There are more examples of pattern matching in action in the
+                Control Flow section of this site.
+              </CodeComparisonDescription>
             </>
+          </Collabsible>
+          <Collabsible title="Control Flow">
+            <CodeComparisonHeader>Case Statements</CodeComparisonHeader>
+            <CodeComparison js={jsCase} elixir={elixirCase} />
+            <CodeComparisonDescription>
+              In addition to case statements, Elixir also has the{" "}
+              <a
+                target="_blank"
+                className="underline"
+                href="https://elixir-lang.org/getting-started/case-cond-and-if.html#cond"
+              >
+                cond
+              </a>{" "}
+              keyword, which is useful for checking different conditions and
+              finding the first one that isn't nil or false, similar to else if
+              statements.
+            </CodeComparisonDescription>
+            <CodeComparisonHeader>If Else Statements</CodeComparisonHeader>
+            <CodeComparison js={jsIf} elixir={elixirIf} />
           </Collabsible>
           <Collabsible title="Common List Operations">
             <>
@@ -75,11 +142,11 @@ export default function Home() {
               <CodeComparison js={jsReduce} elixir={elixirReduce} />
             </>
           </Collabsible>
-          <Collabsible title="Async Tasks">
+          {/* <Collabsible title="Async Tasks">
             <>
               <CodeComparisonHeader>Coming Soon!</CodeComparisonHeader>
             </>
-          </Collabsible>
+          </Collabsible> */}
         </div>
         <Footer />
       </main>
